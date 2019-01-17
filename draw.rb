73810e3c20c3fbc9ec43fa4f9a0594a686c7f59d
Origier <sergeantbeast456@gmail.com>
2019-01-16 20:26:42 -0500

    basic draw function

--- draw.rb
+++ draw.rb
@@ -1,5 +1,6 @@
 require_relative "FlatObject"
 
+grid = Array.new(10) {Array.new(10)}
 triangle = FlatObject.new(0,0)
 
 triangle.addVertex(0,0)
@@ -9,4 +10,15 @@ triangle.connectVertices(triangle.vertices[0],triangle.vertices[1])
 triangle.connectVertices(triangle.vertices[1],triangle.vertices[2])
 triangle.connectVertices(triangle.vertices[2],triangle.vertices[0])
 
-triangle.edges.each{ |edge| puts edge.length}
+def draw(grid)
+  grid.each do |array|
+    (array.length - 1).times do |i|
+      if array[i] == nil
+        print " "
+      else
+        print array[i]
+      end
+    end
+    print "\n"
+  end
+end
