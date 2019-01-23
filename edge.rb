db9e3baf8ebffb73dacabbc39b64c775d095c9cb
Origier <sergeantbeast456@gmail.com>
2019-01-23 16:49:23 -0500

    attempted to correct the removing of doubles, current not working as planned

--- edge.rb
+++ edge.rb
@@ -1,10 +1,12 @@
 class Edge
-  attr_accessor :length, :vertex1, :vertex2, :x_coors, :y_coors, :xincrement, :yincrement
+  attr_accessor :length, :vertex1, :vertex2, :x_coors, :y_coors, :x_coorsf, :y_coorsf, :xincrement, :yincrement
   def initialize(v1, v2)
     @vertex1 = v1      
     @vertex2 = v2
     @x_coors = []
     @y_coors = []
+    @x_coorsf = []
+    @y_coorsf = []
     @x_diff = @vertex1.x_coor - @vertex2.x_coor
     @y_diff = @vertex1.y_coor - @vertex2.y_coor
     @length = (Math.sqrt((@x_diff)**2 + (@y_diff)**2)).round(8)
@@ -19,35 +21,33 @@ class Edge
     until @x.round == @vertex1.x_coor and @y.round == @vertex1.y_coor
       @x += @xincrement
       @y += @yincrement
-      @x_coors << @x.round unless @x.round == @vertex1.x_coor and @y.round == @vertex1.y_coor
-      @y_coors << @y.round unless @x.round == @vertex1.x_coor and @y.round == @vertex1.y_coor
+      @x_coorsf << @x unless @x.round == @vertex1.x_coor and @y.round == @vertex1.y_coor
+      @y_coorsf << @y unless @x.round == @vertex1.x_coor and @y.round == @vertex1.y_coor
     end
-    if x_coors.length > 1
-      removeDoubles
+    if x_coorsf.length > 1
+      roundCoors
     end
   end
 
   private
 
-  def removeDoubles
-    @tempx = [@x_coors[0]]
-    @tempy = [@y_coors[0]]
+  def roundCoors
+    @x_coors = [@x_coorsf[0].round]
+    @y_coors = [@y_coorsf[0].round]
     @i = 1
-    while @i < @x_coors.length
-      @j = 0
-      while @j < @tempx.length
-        if @tempx[@j] == @x_coors[@i] and @tempy[@j] == @y_coors[@i]
-          break
-        else
-          @j += 1
-          next
-        end
-        @tempx << @x_coors[@i]
-        @tempy << @y_coors[@i]
-      end
+    while @i < @x_coorsf.length
+      @x_coors << @x_coorsf[@i].round
+      @y_coors << @y_coorsf[@i].round
       @i += 1
     end
-    @x_coors = @tempx
-    @y_coors = @tempy
+    @i = 0
+    while @i < @x_coors.length
+      if @x_coors[@i] == @x_coors[@i + 1] and @y_coors[@i] == @y_coors[@i - 1]
+        @x_coors.delete_at(@i)
+        @y_coors.delete_at(@i)
+      else 
+        @i += 1
+      end
+    end
   end
 end
